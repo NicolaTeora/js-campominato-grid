25a214cdacf134cb31aad66ea6713ed4c72b5b5d
NicolaTeora <nikxodia@gmail.com>
2024-01-18 20:53:24 +0100

    feat : refresh grid - following lesson

--- JS/main.js
+++ JS/main.js
@@ -2,13 +2,21 @@
 const filedGrid = document.getElementById('field');
 
 //2-richiamo il bottone per riempire il contenitore
-const generatorCell = document.getElementById('generator'); 
+const generatorCellBtn = document.getElementById('generator'); 
 
 //2.1- creo funzione per generare la griglia
 generateCell(filedGrid)//2.2- argomento della funzione sarà il contenitore dove generare l'elemento
 
-//2- creo funzione per generare la griglia
+//4.1- creo la funzione per generare7refreshare la griglia
+//4.2- prima di rigenerare bisognera dire nella funzione di "generazione delle celle" di impostare(2.3) il contenitore come "vuoto" (boxGrid.innerHTML = '';)
+generatorCellBtn.addEventListener('click', function(){
+    generateCell(filedGrid); //4.2- prima di
+})
+
+//2.2- creo funzione per generare la griglia
 function generateCell(boxGrid){
+    boxGrid.innerHTML = ''; //2.3
+
     for (let i = 0; i < 100; i++){
         const newCell = cellCreate(); //3.1- cellCreate() sara la funzione che conterra le istruzioni per creare gli elementi che compongono la cella di riempimento contenitore
         boxGrid.append(newCell); //3.3- dopo aver generato la cella recupero il risultato recupero il contenitore (boxGrid) e l'aggiungo
