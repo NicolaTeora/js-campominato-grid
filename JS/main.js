83de96ab025884a7033643667c3dcaa8140da1d4
NicolaTeora <nikxodia@gmail.com>
2024-01-19 17:15:13 +0100

    feat : add bomb event

--- JS/main.js
+++ JS/main.js
@@ -21,26 +21,31 @@ function generateCell(boxGrid){
     for (let i = 0; i < 100; i++){
         //3.1- cellCreate() sara la funzione che conterra le istruzioni per creare gli elementi che compongono la cella di riempimento contenitore
         const newCell = cellCreate(i); 
-        
-        //3.3- dopo aver generato la cella recupero il risultato recupero il contenitore (boxGrid) e l'aggiungo
+        //3.3- dopo aver generato la cella recupero il risultato, recupero il contenitore (boxGrid) e l'aggiungo
         boxGrid.append(newCell); 
     }
+    
 }
 
 //3.2- creo la funzione per generare gli elementi che compongono la cella
-function cellCreate(boxGrid){
+function cellCreate(index){
     
     //3.2.1 - genero il tag div
     const cell = document.createElement('div');
     
     //3.2.2- gli aggiungo la classe
-    cell.classList.add('cell');  
+    cell.classList.add('cell');
     
     //4- creare la gestione del bottone
     cell.addEventListener('click', function(){
+        console.log(index)
+        if (!bombElements.includes(index)){
+            //4.1- tramite "toggle" dico all'elemento di aggiungere una classe o toglierla se presente
+            cell.classList.toggle('active'); 
+        } else {
+            alert('BOOM!')
+        }
         
-        //4.1- tramite "toggle" dico all'elemento di aggiungere una classe o toglierla se presente
-        cell.classList.toggle('active'); 
     })
     return cell;
 }
